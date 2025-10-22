1afee8a4f9fde5168d199d2a2f7d2420164ef8da
HeleTJ <cshmlx@126.com>
2025-10-22 09:27:25 +0800

    Update rendezvous server and public key values

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["192.168.99.102"];
+pub const RS_PUB_KEY: &str = "2BXamSnjNCh01uYu026LIFbg60M4mr7pm5rEEmnbX18=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
